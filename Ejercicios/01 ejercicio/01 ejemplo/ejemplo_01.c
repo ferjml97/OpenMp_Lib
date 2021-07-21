6497978b2f70e59814aa2c09875516b1e8ad9f91
Fernando Maldonado <47682546+ferjml97@users.noreply.github.com>
2021-07-21 16:52:10 -0500

    Added ejemplo_01
    Un modo de solución del 01 ejercicio

---
+++ Ejercicios/01 ejercicio/01 ejemplo/ejemplo_01.c
@@ -0,0 +1,51 @@
+
+/*                                          EJERCICIO 1
+ *  Ejemplo 1
+ *  Usando la API(OpenMP) hacer un programa que realice lo siguiente:
+ * 	    -   Crear la Matrix A de 50 columnas x 50 filas (50x50), inicializada con valores aleatorios. [✔]
+*/
+
+//  Librerias
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//  Definiciones
+#define CHUNKSIZE   10
+#define N       10
+#define NRA 5               //  numero de filas en matrix A
+#define NCA 5               //  numero de columnas en matrix A
+
+
+//  Metodo para obtener numeros random
+long Random(long li, long ls)
+{ long n;
+    n=li+rand()%(ls-li+1);
+  return n;
+}
+
+//  Ejecucion del programa
+int main (int argc, char *argv[]) {
+
+    int     i, j;
+    double	mA[NRA][NCA];
+
+//  Establece el numero de subprocesos en las proximas regiones paralelas
+omp_set_num_threads(2);
+
+//  Directiva con constructor PARALLEL FOR con clausula SCHEDULE
+#pragma omp parallel for schedule( static,10)
+
+	for (i=0; i<NRA; i++)
+    {
+        for (j=0; j<NCA; j++)
+        {
+            //  Creacion de Matrix A con numeros aleatorios
+            mA[i][j]= Random(1,20);
+            //  Impresion de Matrix A
+            printf("%6.2f   ", mA[i][j]);
+		}
+		printf("\n");
+	}
+
+}   //  Fin main
