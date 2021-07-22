6bf1c283473887af4f5202523545b8f49a1102df
Fernando Maldonado <47682546+ferjml97@users.noreply.github.com>
2021-07-22 03:49:59 -0500

    Modified ejercicio_02
    Modifications to exercise_02

--- Ejercicios/02 ejercicio/ejercicio_02.c
+++ Ejercicios/02 ejercicio/ejercicio_02.c
@@ -1,8 +1,8 @@
 
 /*                                          EJERCICIO 2
  *  Usando la API(OpenMP) hacer un programa que realice lo siguiente:
- * 	    -   Crear la Matrix A de 50 columnas x 50 filas (50x50), inicializada con valores aleatorios.   [✔]
- *      -   Realizar la Transpuesta de la Matrix A.                                                     [✔]
+ * 	    -   Crear la Matriz A de 50 columnas x 50 filas (50x50), inicializada con valores aleatorios.   [✔]
+ *      -   Realizar la Transpuesta de la Matriz A.                                                     [✔]
 */
 
 //  Librerias
@@ -13,8 +13,8 @@
 //  Definiciones
 #define CHUNKSIZE   10
 #define N       10
-#define NRA 5               //  numero de filas en matrix A
-#define NCA 5               //  numero de columnas en matrix A
+#define NRA 5               //  numero de filas en matriz A
+#define NCA 5               //  numero de columnas en matriz A
 
 
 //  Metodo para obtener numeros random
@@ -42,15 +42,15 @@ omp_set_num_threads(2);
     {
         for (j=0; j<NCA; j++)
         {
-            //  Creacion de Matrix A con numeros aleatorios
+            //  Creacion de Matriz A con numeros aleatorios
             mA[i][j]= Random(1,20);
-           //  Las transpuesta de la Matrix A
+           //  Las transpuesta de la Matriz A
             mAT[j][i] = mA[i][j];
 		}
 	}
-    //  Impresion de Matrix A
+    //  Impresion de Matriz A
 	printf("******************************************************\n");
-	printf("Matrix A:\n");
+	printf(" Matriz A:\n");
 	for (i=0; i<NRA; i++)
 	{
         for (j=0; j<NCA; j++)
@@ -59,9 +59,9 @@ omp_set_num_threads(2);
         }
         printf("\n");
 	}
-	//  Impresion Transpuesta Matrix A
+	//  Impresion Transpuesta Matriz A
     printf("******************************************************\n");
-	printf("Matrix Transpuesta de A:\n");
+	printf(" Matriz Transpuesta de A:\n");
 	for (j=0; j<NRA; j++)
 	{
         for (i=0; i<NCA; i++)
